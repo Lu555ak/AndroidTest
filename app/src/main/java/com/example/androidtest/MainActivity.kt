26fb0945cc81134886bfa54dd8b9ebce89df92d9
GeneralBorN <grgecicluka@gmail.com>
2023-02-27 14:43:33 +0100

    Tasks: 3. - 9.

--- app/src/main/java/com/example/androidtest/MainActivity.kt
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.example.androidtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0;
+    lateinit var textViewCounter: TextView
+    lateinit var buttonUp: Button
+    lateinit var buttonDown: Button
+    lateinit var plainTextField: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(applicationContext, "onCreate", Toast.LENGTH_SHORT).show()
         Log.i("MyLog", "onCreate")
+
+        textViewCounter = findViewById(R.id.textViewCounter)
+        buttonUp = findViewById(R.id.buttonUp)
+        buttonDown = findViewById(R.id.buttonDown)
+        plainTextField = findViewById(R.id.plainTextField)
+
+        buttonUp.setOnClickListener() {
+            count++;
+            textViewCounter.text = count.toString()
+
+            if(count >= 10){
+                count = 0
+                val intent = Intent(this, SuccessActivity::class.java)
+                intent.putExtra("name", plainTextField.text.toString())
+                startActivity(intent)
+            }
+        }
+
+        buttonDown.setOnClickListener() {
+            if(count > 0)
+                count--
+            textViewCounter.text = count.toString()
+        }
     }
 
     override fun onStart() {
